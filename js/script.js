983057fc5e3858c83906827077843bec3e9674f1
LucasSpinelli003 <rm97757@fiap.com.br>
2023-04-28 21:00:07 -0300

    Javascript estilizado.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+//console.log("Ola Mundo!!");
+
+var nr1 = "10";
+var nr2 = "5";
+
+//Realizando operações com Sting
+console.log(parseInt(nr1)+parseInt(nr2))
+
+var nome1 = "neymaru";
+// let nome2 = "melin";
+// const nome3 = "igot";
+
+if(nome1 == ""){
+    let nome1 = "igot";
+}
+
+console.log(nome1);
