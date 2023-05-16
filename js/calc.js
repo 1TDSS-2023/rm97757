719a0e1976b535ff04ed4469452b70cfa1e9bda0
LucasSpinelli003 <rm97757@fiap.com.br>
2023-05-15 23:00:04 -0300

    Calculadora arrumada.

--- js/calc.js
+++ js/calc.js
@@ -1,29 +1,29 @@
 //Desafio 1
-
 //Inserir o value dos botões de números e operações no visor.
 //É obrigatório utilizar funções parametrizadas...
-//Pode ser utiizados evneto inline
+//Pode ser utilizados eventos de inline.
 
+//Desafio 3
+//Evite a inserção de mais de um operador por vez no visor.
+function insereNumeroNoVisor(botao,visor) {
 
-function inserirN(botao,visor){ 
-    if(botao.value == "+" || botao.value == "=" || botao.value == "*" || botao.value == "/"){
+    if(botao.value === "+" || botao.value === "-" || botao.value === "*" || botao.value === "/"){
 
-        if(botao.value[visor.value.lenght-2] == "+" || botao.value[visor.value.lenght-2] == "=" || botao.value[visor.value.lenght-2] == "*" || botao.value[visor.value.lenght-2] == "/"){
-        
-            visor.value.slice(visor.value.lenght-1)
+        if(visor.value[visor.value.length-1] === "+" || visor.value[visor.value.length-1] === "-" || visor.value[visor.value.length-1] === "*" || visor.value[visor.value.length-1] === "/"){
+            visor.value.slice(visor.value.length-1);
+        }else{
+            visor.value += botao.value;
         }
 
     }else{
-         visor.value += botao.value;
+        visor.value += botao.value;
     }
 }
+
 //Desafio 2
-//utilizando a função eval(expressão)
-//Crie uma nova função no formato arrowFunction que receba um parâmetro e passe para a função evla() realizar o cálculo e devolva o resultado no visor.
+//Utilizando a função eval(expressão)
+//Crie uma nova função no formato ArrowFunction que receba um parâmetro e passe para a função eval() realizar o cálculo e devolva o resultado no visor.
 
-
-//simplificar o código com arrowFunction
-
-const calculoResultadoOperacoes = (visor)=>{
+const calculaResultadoOperacoes = (visor)=>{
     visor.value = eval(visor.value);
 }
